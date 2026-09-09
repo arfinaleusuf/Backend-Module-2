308d156e6b4a7f61ff35c3681711966d716e0400
arfinaleusuf <arfinaleusuf@gmail.com>
2026-09-10 00:07:28 +0600

    database update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./library.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:arfin123library@db.pqfhsqugdacdnwsrxlht.supabase.co:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind = engine)
