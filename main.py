f0ea9ac83d883e6ae530bfe63859cf9284add426
arfinaleusuf <arfinaleusuf@gmail.com>
2026-09-17 22:47:22 +0600

    try

--- main.py
+++ main.py
@@ -11,23 +11,23 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = [
-#     "http://localhost:5173",
-#     "http://localhost:5174",
-#     "http://127.0.0.1:5173",
-#     "http://127.0.0.1:5174",
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-# ]
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
